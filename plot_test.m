fcb3295a1aa529acc4178198c1781d695558b9fd
Demian <dpoggio@cablevision.com.ar>
2019-10-20 00:09:57 -0300

    Mejora de caso dos a O(log(n))

--- plot_test.m
+++ plot_test.m
@@ -1,16 +1,22 @@
 clear all
 close all
 
-M = csvread('capital_federal.csv');
+M = csvread('capital_federal_conv_neg.csv');
 
-Xmin = 0;
-Ymin = 0;
-Xmax = 17975.40258 ;
-Ymax = 19863.14275;
+Xmin = -9016.359751;
+Ymin = -9904.671966;
+Xmax = 8959.042825 ;
+Ymax = 9958.470781;
+   
+
 sqr = [Xmin Ymin; Xmin Ymax; Xmax Ymax; Xmax Ymin; Xmin Ymin];
 
+
+% (min(M(:,2)) + max(M(:,2))) / 2
 figure
 %plot(c(:,1),c(:,2),sqr(:,1),sqr(:,2))
 plot(M(:,1),M(:,2),sqr(:,1),sqr(:,2))
-
+%  plot(M(:,1) -5643800,M(:,2) -6169100)
+% plot(M(:,1),M(:,2))
+ 
 % 0 0 9854.149231 9900.419522  --> Python
